6ea020ddc9c204d849c23a3707ee34e408abe7e7
Dannymkushwa <107799886+Dannymkushwa@users.noreply.github.com>
2023-12-20 20:18:03 +0530

    #include<stdio.h> int main() {     int n,i,flag=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int a,b,sum=0;     scanf("%d %d",&a,&b);     for(i=0;i<n;i++)     {         if(arr[i]<a||arr[i]>b)         {             printf("%d ",arr[i]);             flag=1;         }     }     if(flag==0)     {         printf("-1");     } }

---
+++ Elements_not_in_between_A_and_B.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,flag=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int a,b,sum=0;
+    scanf("%d %d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]<a||arr[i]>b)
+        {
+            printf("%d ",arr[i]);
+            flag=1;
+        }
+    }
+    if(flag==0)
+    {
+        printf("-1");
+    }
+}
